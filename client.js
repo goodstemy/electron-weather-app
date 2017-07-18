a0027d5223187027caecd72125ecc7406b459752
ivan <baho.39rus@gmail.com>
2017-07-19 01:58:40 +0200

    Remove toolbar; Make toolbar icons to a single standard

--- client.js
+++ client.js
@@ -1,38 +1,67 @@
 const {ipcRenderer} = require('electron');
+const drag = require('electron-drag');
+const remote = require('electron').remote;
 
-const button = document.getElementById('button');
-const cityName = document.getElementById('city-name');
-const output = document.getElementById('output');
-const city = document.getElementById('city');
-const bg = document.getElementById('bg');
+const clear = drag('#drag-area');
 
-button.addEventListener('click', () => {
-	ipcRenderer.send('asynchronous-message', cityName.value);
+if(!drag.supported) {
+    document.querySelector('#drag-area').style['-webkit-app-region'] = 'drag';
+}
+
+const close = document.querySelector("#close");
+const expand = document.querySelector("#expand");
+const turn = document.querySelector("#turn");
+
+close.addEventListener("click", (e) => {
+	const window = remote.getCurrentWindow();
+	window.close();
 });
 
-cityName.addEventListener('keyup', (event) => {
-	if (event.keyCode === 13) {
-		ipcRenderer.send('asynchronous-message', cityName.value);
-
-		cityName.value = "";
-		city.style.opacity = 0;
-		output.style.opacity = 0;
-	}
-});
-
-// ipcRenderer.send('asynchronous-message', 'go');
-
-ipcRenderer.on('asynchronous-reply', (event, arg) => {
-	console.log(arg);
-
-	if (!!arg.res[0]) {
-		city.innerHTML = arg.res[0].current.observationpoint + ":"
-		output.innerHTML = arg.res[0].current.temperature + "℃"
-
-		city.style.opacity = 1;
-		output.style.opacity = 1;
-		bg.style.backgroundImage = 'url("' + arg.img[0].url + '")'
+expand.addEventListener("click", (e) => {
+	const window = remote.getCurrentWindow();
+	if (!window.isMaximized()) {
+		window.maximize();
 	} else {
-		output.innerHTML = "Can not found city with name " + cityName.value
+		window.unmaximize();
 	}
 });
+
+turn.addEventListener("click", (e) => {
+	const window = remote.getCurrentWindow();
+	window.minimize();
+});
+
+// const button = document.getElementById('button');
+// const cityName = document.getElementById('city-name');
+// const output = document.getElementById('output');
+// const city = document.getElementById('city');
+// const bg = document.getElementById('bg');
+
+// button.addEventListener('click', () => {
+// 	ipcRenderer.send('asynchronous-message', cityName.value);
+// });
+
+// cityName.addEventListener('keyup', (event) => {
+// 	if (event.keyCode === 13) {
+// 		ipcRenderer.send('asynchronous-message', cityName.value);
+
+// 		cityName.value = "";
+// 		city.style.opacity = 0;
+// 		output.style.opacity = 0;
+// 	}
+// });
+
+// ipcRenderer.on('asynchronous-reply', (event, arg) => {
+// 	console.log(arg);
+
+// 	if (!!arg.res[0]) {
+// 		city.innerHTML = arg.res[0].current.observationpoint + ":"
+// 		output.innerHTML = arg.res[0].current.temperature + "℃"
+
+// 		city.style.opacity = 1;
+// 		output.style.opacity = 1;
+// 		bg.style.backgroundImage = 'url("' + arg.img[0].url + '")'
+// 	} else {
+// 		output.innerHTML = "Can not found city with name " + cityName.value
+// 	}
+// });
