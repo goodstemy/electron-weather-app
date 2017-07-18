edbc20653afc27502b321412e48b3c8c82a04461
rob3n <roben1006@gmail.com>
2017-07-18 14:54:17 +0300

    rework

---
+++ static/react/Components/Weather.js
@@ -0,0 +1,46 @@
+export default function() {
+  return (
+    <div class="weather">
+      <div class="weather-item">
+        <div class="weather-statistics-text">
+          <div class="weather-statistic-temperature">82&deg;</div>
+          <div class="weather-statistic-day">Monday</div>
+        </div>
+        <div class="weather-statistic-data">
+          <i class="fa fa-cloud fa-4x" aria-hidden="true" />
+          <div class="weather-statistic-data-text">4mph/ 67&deg;</div>
+        </div>
+      </div>
+      <div class="weather-item">
+        <p class="weather-text-day">Tuesday</p>
+        <i class="fa fa-cloud fa-2x" aria-hidden="true" />
+        <p class="weather-text-temperature">82&deg;</p>
+      </div>
+      <div class="weather-item">
+        <p class="weather-text-day">Tuesday</p>
+        <i class="fa fa-cloud fa-2x" aria-hidden="true" />
+        <p class="weather-text-temperature">82&deg;</p>
+      </div>
+      <div class="weather-item">
+        <p class="weather-text-day">Tuesday</p>
+        <i class="fa fa-cloud fa-2x" aria-hidden="true" />
+        <p class="weather-text-temperature">82&deg;</p>
+      </div>
+      <div class="weather-item">
+        <p class="weather-text-day">Tuesday</p>
+        <i class="fa fa-cloud fa-2x" aria-hidden="true" />
+        <p class="weather-text-temperature">82&deg;</p>
+      </div>
+      <div class="weather-item">
+        <p class="weather-text-day">Tuesday</p>
+        <i class="fa fa-cloud fa-2x" aria-hidden="true" />
+        <p class="weather-text-temperature">82&deg;</p>
+      </div>
+      <div class="weather-item">
+        <p class="weather-text-day">Tuesday</p>
+        <i class="fa fa-cloud fa-2x" aria-hidden="true" />
+        <p class="weather-text-temperature">82&deg;</p>
+      </div>
+    </div>
+  );
+}
