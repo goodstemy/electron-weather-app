8199d84136e87c90524f82e32bce3485c3cef471
rob3n <roben1006@gmail.com>
2017-07-18 14:54:17 +0300

    rework

---
+++ static/react/Components/Header.jsx
@@ -0,0 +1,14 @@
+export default function() {
+  return (
+    <header class="header">
+      <div class="header-search">
+        <input type="text" class="header-input-search" />
+        <button class="header-button-search">Search</button>
+      </div>
+      <div class="header-location">
+        <div class="header-location-city">Florida USA</div>
+        <div class="header-location-time">20:15 pm</div>
+      </div>
+    </header>
+  );
+}
