9a05b5030a9302847d5375c67a4b05a5935c923e
ivan <baho.39rus@gmail.com>
2017-07-23 00:12:35 +0200

    Add basic search functional; Refactor code

--- client.js
+++ client.js
@@ -1,18 +1,19 @@
-(function() {
-
 const {ipcRenderer} = require('electron');
 const drag = require('electron-drag');
 const remote = require('electron').remote;
 
 const clear = drag('#drag-area');
 
-if(!drag.supported) {
-  document.querySelector('#drag-area').style['-webkit-app-region'] = 'drag';
-}
-
 const close = document.querySelector(".button-close");
 const expand = document.querySelector(".button-close");
 const turn = document.querySelector(".button-turn");
+const image = document.querySelector(".header-background-img");
+const weather = document.querySelector(".weather");
+const search = document.querySelector('.header-input-search');
+
+if(!drag.supported) {
+  document.querySelector('#drag-area').style['-webkit-app-region'] = 'drag';
+}
 
 close.addEventListener("click", (e) => {
 	const window = remote.getCurrentWindow();
@@ -33,9 +34,6 @@ turn.addEventListener("click", (e) => {
 	window.minimize();
 });
 
-const image = document.querySelector(".header-background-img");
-const weather = document.querySelector(".weather");
-
 image.addEventListener('load', function() {
 	const vibrant = new Vibrant(image);
 	const swatches = vibrant.swatches();
@@ -48,6 +46,18 @@ image.addEventListener('load', function() {
 	}
 
 	weather.style.background = getGradient(gradient);
+});
+
+search.addEventListener('keyup', (e) => {
+	if (e.keyCode === 13 && !!search.value) {
+		ipcRenderer.send('asynchronous-message', search.value);
+
+		search.value = "";
+	}
+});
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+	console.log(arg);
 });
 
 function prettifyRgb(gradient, rgb) {
@@ -67,44 +77,4 @@ function getGradient(gradient) {
 	return style;
 }
 
-})();
-// img.addEventListener('load', function() {
-	// var vibrant = new Vibrant(img);
-	// var swatches = vibrant.swatches();
-	// console.log(swatches);
-// });
 
-// const button = document.getElementById('button');
-// const cityName = document.getElementById('city-name');
-// const output = document.getElementById('output');
-// const city = document.getElementById('city');
-// const bg = document.getElementById('bg');
-
-// button.addEventListener('click', () => {
-// 	ipcRenderer.send('asynchronous-message', cityName.value);
-// });
-
-// cityName.addEventListener('keyup', (event) => {
-// 	if (event.keyCode === 13) {
-// 		ipcRenderer.send('asynchronous-message', cityName.value);
-
-// 		cityName.value = "";
-// 		city.style.opacity = 0;
-// 		output.style.opacity = 0;
-// 	}
-// });
-
-// ipcRenderer.on('asynchronous-reply', (event, arg) => {
-// 	console.log(arg);
-
-// 	if (!!arg.res[0]) {
-// 		city.innerHTML = arg.res[0].current.observationpoint + ":"
-// 		output.innerHTML = arg.res[0].current.temperature + "℃"
-
-// 		city.style.opacity = 1;
-// 		output.style.opacity = 1;
-// 		bg.style.backgroundImage = 'url("' + arg.img[0].url + '")'
-// 	} else {
-// 		output.innerHTML = "Can not found city with name " + cityName.value
-// 	}
-// });
--- main.js
+++ main.js
@@ -12,11 +12,40 @@ const BrowserWindow = electron.BrowserWindow;
 
 const client = new GoogleImages('001462481511008949931:vvhugxvyp6u', 'AIzaSyBSljMrRqro1HpWZobmnAHddw2bNtZOyMU');
 
+let mainWindow;
+
+// Only for dev
 require('electron-reload')(__dirname, {
 	electron: require('electron-prebuilt')
 });
 
-let mainWindow;
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+	weather.find({search: arg, degreeType: 'C'}, (err, result) => {
+		if (err) console.log(err);
+
+		client.search(arg)
+			.then(images => {
+				event.sender.send('asynchronous-reply', {res: result, img: images})
+			})
+			.catch(err => {
+				console.log(err);
+			});
+		});
+});
 
 function createWindow() {
 	mainWindow = new BrowserWindow({
@@ -38,36 +67,7 @@ function createWindow() {
 
 	mainWindow.webContents.openDevTools()
 
-	ipcMain.on('asynchronous-message', (event, arg) => {
-
-  	weather.find({search: arg, degreeType: 'C'}, (err, result) => {
-  		if (err) console.log(err);
-
-			client.search(arg)
-				.then(images => {
-  				event.sender.send('asynchronous-reply', {res: result, img: images})
-				})
-				.catch(err => {
-					console.log(err);
-				});
-  		});
-	});
-
 	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
 }
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-app.on('activate', () => {
-	if (mainWindow === null) {
-		createWindow();
-	}
-});
